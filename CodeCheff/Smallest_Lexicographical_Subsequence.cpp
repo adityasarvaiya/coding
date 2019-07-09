d6bab03ae2af3d50403c46b76b0dd831563a1885
adityasarvaiya <adityasarvaiya96@gmail.com>
2019-07-10 01:16:13 +0530

    Codecheff

---
+++ CodeCheff/Smallest_Lexicographical_Subsequence.cpp
@@ -0,0 +1,50 @@
+// https://www.codechef.com/submit/ASTRING
+// https://www.codechef.com/problems/ASTRING
+// https://discuss.codechef.com/t/astring-editorial/12597
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void util(string s, long long k) {
+    set < pair<char, long long> > h;
+    long long i, n = s.length();
+    
+    for (i = 0; i < n-k+1; ++i) {
+        h.insert({s[i], i});
+    }
+    
+    // string ans = "";
+    int start, deleted_till = 0;
+    
+    for (; i < n; ++i) {
+        // ans = ans + (*h.begin()).first;
+        cout << (*h.begin()).first;
+        start = (*h.begin()).second;
+        
+        while (deleted_till <= start) {
+            h.erase({s[deleted_till], deleted_till});
+            deleted_till++;
+        }
+        
+        h.insert({s[i], i});
+    }
+    
+    // ans = ans + (*h.begin()).first;
+    cout << (*h.begin()).first << endl;
+    // return ans;
+}
+
+int main() {
+    long long  t, k;
+    cin >> t;
+    
+    while (t--) {
+        string s;
+        cin >> s >> k;
+        
+        util(s,k);
+        // cout << util(s,k) << endl;
+    }
+
+	return 0;
+}
